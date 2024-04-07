161f795af8e217a2e7c6b2ede2b01488773c4b5a
a8ana <a8anassis@gmail.com>
2024-04-07 15:41:44 +0300

    -- ch2

--- src/gr/aueb/cf/ch2/OutputApp.java
+++ src/gr/aueb/cf/ch2/OutputApp.java
@@ -14,13 +14,13 @@ public class OutputApp {
         int num = 1;
         long num2 = 202345676;
 
-       /* System.out.println("Hello");
-        System.out.print("Hello2");
-        System.out.println();
-        System.out.printf("Hello");
-
-        System.out.println("Num is: " + num);
-        System.out.printf("Num is %d", num);*/
+//        System.out.println("Hello");
+//        System.out.print("Hello2");
+//        System.out.println();
+//        System.out.printf("Hello");
+//
+//        System.out.println("Num is: " + num);
+//        System.out.printf("Num is %d", num);
 
         System.out.printf(Locale.forLanguageTag("el"),  "Num is: %04d, Num2 is: %,024d", num, num2);
     }
