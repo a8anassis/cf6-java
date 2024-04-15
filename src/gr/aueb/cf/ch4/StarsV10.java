7952b0142a58715a1f063c497cc1e7d22b722df1
a8ana <a8anassis@gmail.com>
2024-04-15 20:31:24 +0300

    Stars10 Vertical

---
+++ src/gr/aueb/cf/ch4/StarsV10.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.ch4;
+
+/**
+ * Εκτυπώνει 10 κάθετα αστεράκια.
+ */
+public class StarsV10 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("*");
+        }
+    }
+}
