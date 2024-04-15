a2a8a800ae3388ffd8d575bedcc47ac60b016b40
a8ana <a8anassis@gmail.com>
2024-04-15 20:26:15 +0300

    Βιγ Ιντ

---
+++ src/gr/aueb/cf/ch4/BigIntPowerApp.java
@@ -0,0 +1,27 @@
+package gr.aueb.cf.ch4;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * Υπολογίζει το a^b με BigInteger
+ */
+public class BigIntPowerApp {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BigInteger base = BigInteger.ZERO;
+        BigInteger power = BigInteger.ZERO;
+        BigInteger result = BigInteger.ONE;
+
+        System.out.println("Please insert two integers");
+        base = BigInteger.valueOf(in.nextInt());
+        power = BigInteger.valueOf(in.nextInt());
+
+        for (int i = 1; i <= power.intValue(); i++) {
+            result = result.multiply(base);
+        }
+
+        System.out.printf("%d^%d = %,d", base, power, result);
+    }
+}
