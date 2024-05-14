afdceb484edbcea012fac861d82f9d891b38e45b
a8ana <a8anassis@gmail.com>
2024-05-14 20:52:47 +0300

    Bulk copy of raw data (buffered)

---
+++ src/gr/aueb/cf/ch9/ImageBufferedCopyApp.java
@@ -0,0 +1,33 @@
+package gr.aueb.cf.ch9;
+
+import java.io.*;
+
+public class ImageBufferedCopyApp {
+
+    public static void main(String[] args) {
+        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream("C:/tmp/museum-in.pdf"));
+             BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("C:/tmp/museum-out.pdf")) ) {
+
+            int b;
+            int count = 0;
+            long start;
+            long end;
+            double elapsedTime = 0.0;
+            byte[] buf = new byte[4096];
+
+            start = System.currentTimeMillis();
+            while ((b = bis.read(buf, 0, buf.length)) != -1) {
+                bos.write(b);
+                count += b;
+            }
+            end = System.currentTimeMillis();
+            elapsedTime = (end - start) / 1000.0;
+
+            System.out.printf("Το αρχείο με μέγεθος %dΚΒ (%d bytes) αντιγράφηκε επιτυχώς", (count / 1024), count);
+            System.out.println("Elapsed time: " + elapsedTime + " seconds");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
