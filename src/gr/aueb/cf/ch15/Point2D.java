b81ef247aef88cd573eea1d6875a33d617aec98e
Th. Androutsos <a8anassis@gmail.com>
2024-06-04 18:56:41 +0300

    Point Inheritance

--- src/gr/aueb/cf/ch15/Point2D.java
+++ src/gr/aueb/cf/ch15/Point2D.java
@@ -25,4 +25,22 @@ public class Point2D extends Point {
     public String convertToString() {
         return "(" + getX() + ", " + y + ")";
     }
+
+    @Override
+    public void movePlus10() {
+        super.movePlus10();
+        y += 10;
+    }
+
+    @Override
+    public void movePlusOne() {
+        super.movePlusOne();
+        y += 1;
+    }
+
+    @Override
+    protected void reset() {
+        super.reset();
+        y = 0;
+    }
 }
---
+++ src/gr/aueb/cf/ch15/Point3D.java
@@ -0,0 +1,46 @@
+package gr.aueb.cf.ch15;
+
+public class Point3D extends Point2D {
+    private double z;
+
+    public Point3D() {
+//        super();
+//        z = 0;
+    }
+
+    public Point3D(double x, double y, double z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    @Override
+    public String convertToString() {
+        return "(" + getX() + ", " + getY() + ", " + z + ")";
+    }
+
+    @Override
+    public void movePlus10() {
+        super.movePlus10();
+        z += 10;
+    }
+
+    @Override
+    public void movePlusOne() {
+        super.movePlusOne();
+        z += 1;
+    }
+
+    @Override
+    protected void reset() {
+        super.reset();
+        z = 0;
+    }
+}
