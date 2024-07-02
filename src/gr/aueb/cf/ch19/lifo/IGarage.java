dbc12f88b228af2571419330d14b8cd65118bfb5
Th. Androutsos <a8anassis@gmail.com>
2024-07-02 19:36:43 +0300

    add garage interface

---
+++ src/gr/aueb/cf/ch19/lifo/IGarage.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.ch19.lifo;
+
+public interface IGarage {
+    void addCar(String car) throws GarageIsFullException;
+    String removeCar() throws GarageIsEmptyException;
+    String[] toArray(String[] a);
+    void printCars();
+    boolean isFull();
+    boolean isEmpty();
+}
