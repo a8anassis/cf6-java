4aaa7f47e583045ed092823b9b4c1bc87e53a7da
Th. Androutsos <a8anassis@gmail.com>
2024-07-10 20:54:36 +0300

    update with buttons

--- src/gr/aueb/cf/ch21/CodingFrame.java
+++ src/gr/aueb/cf/ch21/CodingFrame.java
@@ -1,6 +1,8 @@
 package gr.aueb.cf.ch21;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CodingFrame extends JFrame {
 
@@ -9,5 +11,32 @@ public class CodingFrame extends JFrame {
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Hello Coding Factory");
+
+        JPanel contentPane = new JPanel();
+        this.setContentPane(contentPane);
+
+        JButton messageBtn = new JButton("Message Button");
+        messageBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "This is a message", "Title of Message", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+
+        JButton confirmBtn = new JButton("Confirm Button");
+        confirmBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int answer = JOptionPane.showConfirmDialog(null, "Confirm message", "Confirm Title", JOptionPane.YES_NO_OPTION);
+                if (answer == JOptionPane.YES_OPTION) {
+                    JOptionPane.showMessageDialog(null, "Yes message", "Yes Title", JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Error Message", "Error Title", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        contentPane.add(messageBtn);
+        contentPane.add(confirmBtn);
     }
 }
