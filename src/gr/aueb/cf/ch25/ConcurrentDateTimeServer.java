5d1af8b40c0625e3ea64486a27c9ffa79994a7dc
Th. Androutsos <a8anassis@gmail.com>
2024-07-30 19:03:10 +0300

    concurrent date time server added

---
+++ src/gr/aueb/cf/ch25/ConcurrentDateTimeServer.java
@@ -0,0 +1,26 @@
+package gr.aueb.cf.ch25;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Date;
+
+public class ConcurrentDateTimeServer implements Runnable {
+
+    private final Socket sockFd;
+
+    public ConcurrentDateTimeServer(Socket sockFd) {
+        this.sockFd = sockFd;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(sockFd.getOutputStream()))) {
+            bw.write(new Date().toString());
+            bw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
